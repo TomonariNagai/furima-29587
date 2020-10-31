4ab0c2d986278f9095fe055abaf0c4072a259eef
TomonariNagai <naga.tomo19931231@gmail.com>
2020-10-31 14:12:34 +0900

    [add]バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: {message: "can't be blank"}
+  validates :email, uniqueness: true, format: {with: /@.+/, allow_blank: true}
+  validates :password, length: {minimum: 6 }, confirmation: {message: "confirmation doesn't match Password"}, format: {with: /\A[a-zA-Z0-9]+\z/, message: "include both letters and numbers"}
+  validates :firstname, presence: {message: "can't be blank"}, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
+  validates :lastname, presence: {message: "can't be blank"}, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
+  validates :firstname_kana, presence: {message: "can't be blank"}, format: {with: /\A[ァ-ヶー－]+\z/, message: "Full-width characters"}
+  validates :lastname_kana, presence: {message: "can't be blank"}, format: {with: /\A[ァ-ヶー－]+\z/, message: "Full-width characters"}
+  validates :birthday, presence: {message: "can't be blank"}
+
 end
--- db/migrate/20201025060344_devise_create_users.rb
+++ db/migrate/20201025060344_devise_create_users.rb
@@ -6,12 +6,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :nickname, null: false
-      t.string :firstname, null: false
-      t.string :lastname, null: false
-      t.string :firstname_kana, null: false
-      t.string :lastname_kana, null: false
-      t.date :birthday, null: false
+      t.string :nickname
+      t.string :firstname
+      t.string :lastname
+      t.string :firstname_kana
+      t.string :lastname_kana
+      t.date :birthday
 
       ## Recoverable
       t.string   :reset_password_token
