264a54fd4b371094e98afdb39cebfea32e070a3f
TomonariNagai <naga.tomo19931231@gmail.com>
2020-11-01 16:07:19 +0900

    [add]バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: {message: "can't be blank"}
+  validates :nickname, presence: true
   validates :email, uniqueness: true, format: {with: /@.+/, allow_blank: true}
-  validates :password, length: {minimum: 6 }, confirmation: {message: "confirmation doesn't match Password"}, format: {with: /\A[a-zA-Z0-9]+\z/, message: "include both letters and numbers"}
-  validates :firstname, presence: {message: "can't be blank"}, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
-  validates :lastname, presence: {message: "can't be blank"}, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
-  validates :firstname_kana, presence: {message: "can't be blank"}, format: {with: /\A[ァ-ヶー－]+\z/, message: "Full-width characters"}
-  validates :lastname_kana, presence: {message: "can't be blank"}, format: {with: /\A[ァ-ヶー－]+\z/, message: "Full-width characters"}
-  validates :birthday, presence: {message: "can't be blank"}
-
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "include both letters and numbers"}
+  validates :firstname, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
+  validates :lastname, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
+  validates :firstname_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "Full-width characters"}
+  validates :lastname_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "Full-width characters"}
+  validates :birthday, presence: true
 end
