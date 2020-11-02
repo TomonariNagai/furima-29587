c7b9f9b6cbbc103db53af7dde3680f79a9a92b86
TomonariNagai <naga.tomo19931231@gmail.com>
2020-11-02 20:53:00 +0900

    [clean]バリデーションのリファクタリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
+  NAMEKANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+
   validates :email, uniqueness: true, format: {with: /@.+/, allow_blank: true}
   validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "include both letters and numbers"}
-  validates :firstname, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
-  validates :lastname, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
-  validates :firstname_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "Full-width characters"}
-  validates :lastname_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "Full-width characters"}
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    
+    with_options format: {with: NAME_REGEX, message: "Full-width characters"} do
+      validates :firstname
+      validates :lastname
+    end
+
+    with_options format: {with: NAMEKANA_REGEX, message: "Full-width katakana characters"} do
+      validates :firstname_kana
+      validates :lastname_kana
+    end
+  end
 end
