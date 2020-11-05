5c10dfcea082ab767fa54d9f8bcfae73c4ff3e17
TomonariNagai <naga.tomo19931231@gmail.com>
2020-11-05 20:51:05 +0900

    [add]商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,5 +5,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :text, :category_id, :condition_id, :shipping_fee_id, :shipping_from_id, :shipping_date_id, :price, :image).merge(user_id: current_user.id)
   end
 end
