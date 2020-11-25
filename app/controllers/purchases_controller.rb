20d2482488d220c951804e4a0ca83fc4ed1ab6c5
TomonariNagai <naga.tomo19931231@gmail.com>
2020-11-25 21:27:41 +0900

    [add]商品購入機能の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,4 +2,23 @@ class PurchasesController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
   end
+
+  def new
+  end
+
+  def create
+    purchase = Purchase.create(purchase_params)
+    Delivery.create(delivery_params(purchase))
+    redirect_to action: :index
+  end
+
+  private
+
+  def purchase_params
+    params.permit().merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def delivery_params(purchase)
+    params.permit(:postal_code, :prefecture_id, :municipality, :house_num, :building, :phone_num).merge(purchase_id: purchase.id)
+  end
 end
