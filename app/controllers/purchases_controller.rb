e9a600b0cddb1fd8deffd8a3732cedb6745828da
TomonariNagai <naga.tomo19931231@gmail.com>
2020-11-29 16:24:34 +0900

    [clean]リファクタリング

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -8,12 +8,7 @@ class PurchasesController < ApplicationController
     @item = Item.find(params[:item_id])
     @purchase_delivery = PurchaseDelivery.new(delivery_params)
       if @purchase_delivery.valid?
-        Payjp.api_key = "sk_test_6339c1ac0d3d22dda2dc143c"
-        Payjp::Charge.create(
-          amount: @item.price,
-          card: delivery_params[:token],
-          currency: 'jpy'
-        )
+        pay_item
         @purchase_delivery.save
         redirect_to root_path
       else
@@ -25,5 +20,14 @@ class PurchasesController < ApplicationController
 
   def delivery_params
     params.require(:purchase_delivery).permit(:postal_code, :prefecture_id, :municipality, :house_num, :building, :phone_num).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_6339c1ac0d3d22dda2dc143c"
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: delivery_params[:token],
+      currency: 'jpy'
+    )
   end
 end
