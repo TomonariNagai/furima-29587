0165f2ef59fa106215ad051038421f4fced88a80
TomonariNagai <naga.tomo19931231@gmail.com>
2020-11-29 22:56:53 +0900

    [add]ユーザー認証機能の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,11 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+
   def index
     @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
     @purchase_delivery = PurchaseDelivery.new
   end
 
